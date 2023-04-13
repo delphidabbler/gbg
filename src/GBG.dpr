14434a3e75c0317c876e6974b27c3a358eb4ecab
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2023-04-14 00:24:08 +0100

    Add program skeleton to GBG.dpr
    * Reports memory leaks in debug mode.
    
    * Adds message and requires enter to be pressed at end of program iff:
      * debug build and
      * running under IDE debugger and
      * running on Windows
    
    * Adds last chance exception handling.

--- src/GBG.dpr
+++ src/GBG.dpr
@@ -4,6 +4,30 @@ program GBG;
 
 {$R *.res}
 
+uses
+  System.SysUtils;
 
 begin
+  {$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := True;
+  {$ENDIF}
+  try
+    {$IF Defined(DEBUG) and Defined(MSWINDOWS)}
+    {$WARN SYMBOL_PLATFORM OFF}
+    if DebugHook <> 0 then
+    begin
+      Writeln;
+      Writeln('Press enter to end the program');
+      Readln;
+    end;
+    {$WARN SYMBOL_PLATFORM ON}
+    {$ENDIF}
+  except
+    on E: Exception do
+    begin
+      Writeln('Uncaught exception:');
+      Writeln(E.ClassName + ': ' + E.Message);
+    end;
+  end;
 end.
+
