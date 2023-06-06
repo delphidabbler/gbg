bf511060f4b0f0eca02ce9975a4f583688e8013a
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2023-06-06 08:07:20 +0100

    Refactor TPrintableASCIIGarbageGenerator.FillBuffer
    Remove unused consts
    Remove intermediate Byte variable

--- src/GBG.Generator.ASCIIGarbage.pas
+++ src/GBG.Generator.ASCIIGarbage.pas
@@ -25,19 +25,12 @@ uses
 { TPrintableASCIIGarbageGenerator }
 
 procedure TPrintableASCIIGarbageGenerator.FillBuffer(var Buffer: TBytes);
-const
-  LF = 10;
-  SPACE = 32;
-  DEL = 127;
 begin
   Randomize;
   for var Idx: UInt64 := 0 to Pred(Length(Buffer)) do
-  begin
-    // Get ASCII code in range 32..127 (SPACE..DEL)
-    // (2nd param of RandomRange is exclusive)
-    var B := Byte(RandomRange($20, Succ($7E)));
-    Buffer[Idx] := B;
-  end;
+    // Get ASCII code in range $20..$7E (SPACE..'~')
+    // (2nd param of RandomRange is exclusive, hence use of Succ())
+    Buffer[Idx] := Byte(RandomRange($20, Succ($7E)));
 end;
 
 { TASCIIGarbageGenerator }
