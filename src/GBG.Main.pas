59ff8d581d7c1541e63ca4ac26968358b7b16742
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2024-08-30 20:36:02 +0100

    Document new -l & -L options
    Update Usage section of README.md and program's help screen.

--- src/GBG.Main.pas
+++ src/GBG.Main.pas
@@ -290,6 +290,8 @@ begin
   WriteLn('      -A -> generate all ASCII characters (code 0..127)');
   WriteLn('      -o -> stop with error if output file already exists');
   WriteLn('      -O -> silently overwrite existing output file with same name');
+  WriteLn('      -l -> stop with error if requested file size > 500Mb');
+  WriteLn('      -L -> silently create file of any size, ignoring 500Mb limit');
   WriteLn;
   WriteLn('    -V = display version information and halt');
   WriteLn;
